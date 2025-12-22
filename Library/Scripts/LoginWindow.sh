75bbac9300797a0ce1ed2d4e5ac1490f21c6b49a
probonopd <probonopd@users.noreply.github.com>
2025-12-22 20:57:29 +0100

    Add LoginWindow.sh

---
+++ Library/Scripts/LoginWindow.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+. /System/Library/Makefiles/GNUstep.sh
+
+# Add our fonts path to fontconfig
+export FONTCONFIG_PATH=/System/Library/Preferences
+export FONTCONFIG_FILE=$FONTCONFIG_PATH/fonts.conf
+
+LoginWindow
