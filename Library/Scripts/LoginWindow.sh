a8458ab38097e92dee94f8ff2cf286e6d338d5c8
probonopd <probonopd@users.noreply.github.com>
2026-01-17 13:28:07 +0100

    Allow non-root users to power off, halt, and reboot

--- Library/Scripts/LoginWindow.sh
+++ Library/Scripts/LoginWindow.sh
@@ -4,6 +4,9 @@
 
 . /System/Library/Makefiles/GNUstep.sh
 
+# Allow non-root users to power off, halt, and reboot the system
+for bin in /sbin/poweroff /sbin/halt /sbin/reboot; do [ -e "$bin" ] && chmod 5755 "$bin"; done
+
 # Add our fonts path to fontconfig
 export FONTCONFIG_PATH=/System/Library/Preferences
 export FONTCONFIG_FILE=$FONTCONFIG_PATH/fonts.conf
